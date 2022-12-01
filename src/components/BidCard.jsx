c5af0efd7cc3bbb4ef96de5f20160b4b3a6785f6
parthpandyappp <parthpandyappp@gmail.com>
2022-12-01 19:52:17 +0530

    Design: Auth pages

--- src/components/BidCard.jsx
+++ src/components/BidCard.jsx
@@ -3,34 +3,29 @@ import React from "react";
 const BidCard = ({ data }) => {
   const { title, description, upload_file, base_price } = data;
   return (
-    <div class="w-80 h-1/2 bg-white  rounded-lg">
-      <a href="#" className="">
-        <img
-          class="rounded-t-lg object-cover h-72 w-80"
-          src={upload_file}
-          alt=""
-        />
-      </a>
-      <div class="p-5 border border-t-0 h-64 w-80">
-        <a href="#" class="mb-3">
-          <h5 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            {title}
-          </h5>
-          <p class="font-medium text-xs text-gray-500">
-            Starts at ${base_price}
-          </p>
-        </a>
-        <p class="mt-4 mb-3 font-normal text-gray-700 dark:text-gray-400 w-80 h-24">
+    <div className="w-80 h-1/2 bg-white  rounded-lg">
+      <img
+        className="rounded-t-lg object-cover h-72 w-80"
+        src={upload_file}
+        alt=""
+      />
+
+      <div className="p-5 border border-t-0 h-64 w-80">
+        <h5 className="mb-3 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          {title}
+        </h5>
+        <p className="font-medium text-xs text-gray-500">
+          Starts at ${base_price}
+        </p>
+
+        <p className="mt-4 mb-3 font-normal text-gray-700 dark:text-gray-400 w-80 h-24">
           {description.slice(0, 90)}...
         </p>
-        <a
-          href="#"
-          class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 self-end"
-        >
+        <p className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 self-end">
           Learn more
           <svg
             aria-hidden="true"
-            class="w-4 h-4 ml-2 -mr-1"
+            className="w-4 h-4 ml-2 -mr-1"
             fill="currentColor"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
@@ -41,7 +36,7 @@ const BidCard = ({ data }) => {
               clip-rule="evenodd"
             ></path>
           </svg>
-        </a>
+        </p>
       </div>
     </div>
   );
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
 export { Nav } from "./Nav"
 export { Footer } from "./Footer"
-export {BidCard} from "./BidCard"
+export { BidCard } from "./BidCard"
