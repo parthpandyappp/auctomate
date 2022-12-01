4ba3c57f70154465090233a73720399cba33c4b2
parthpandyappp <parthpandyappp@gmail.com>
2022-12-01 18:14:58 +0530

    [Design]: Universal Product Dir

---
+++ src/components/BidCard.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+const BidCard = ({ data }) => {
+  const { title, description, upload_file, base_price } = data;
+  return (
+    <div class="w-80 h-1/2 bg-white  rounded-lg">
+      <a href="#" className="">
+        <img
+          class="rounded-t-lg object-cover h-72 w-80"
+          src={upload_file}
+          alt=""
+        />
+      </a>
+      <div class="p-5 border h-64 w-80">
+        <a href="#" class="mb-3">
+          <h5 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {title}
+          </h5>
+          <p class="font-medium text-xs text-gray-500">
+            Starts at ${base_price}
+          </p>
+        </a>
+        <p class="mt-4 mb-3 font-normal text-gray-700 dark:text-gray-400 w-80 h-24">
+          {description.slice(0, 90)}...
+        </p>
+        <a
+          href="#"
+          class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 self-end"
+        >
+          Learn more
+          <svg
+            aria-hidden="true"
+            class="w-4 h-4 ml-2 -mr-1"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export { BidCard };
--- src/pages/BiddingList.jsx
+++ src/pages/BiddingList.jsx
@@ -1,7 +1,18 @@
+import { BidCard } from "../components";
+import { bids } from "../mock data/db-bids";
+
 const BiddingList = () => {
   return (
-    <div className="flex justify-center items-center w-full grow">
-      <h1 className="text-3xl font-bold">All the bids will be shown here</h1>
+    <div className="flex flex-col justify-center items-center w-full grow p-24">
+      <div className="mb-4">
+        <h1 className="font-bold text-2xl underline underline-offset-8 ">
+          Universal Product Directory
+        </h1>
+      </div>
+
+      <div className="flex flex-wrap gap-4 justify-center items-center">
+        {bids && bids.map((bid) => <BidCard key={bid.reg_ref} data={bid} />)}
+      </div>
     </div>
   );
 };
