ba4e4862167ca95750e9a199b700028583de8759
parthpandyappp <parthpandyappp@gmail.com>
2023-02-23 19:52:33 +0530

    minor alterations

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
 export { Nav } from "./Nav"
 export { Footer } from "./Footer"
 export { BidCard } from "./BidCard"
-export { LearnMoreAboutBid } from "./LearnMoreAboutBid"
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,3 +3,4 @@ export { Login } from "./Login";
 export { Signup } from "./Signup";
 export { BiddingList } from "./BiddingList";
 export { AssetRegForm } from "./AssetRegForm";
+export { LearnMoreAboutBid } from "./LearnMoreAboutBid"
